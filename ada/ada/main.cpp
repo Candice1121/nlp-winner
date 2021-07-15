d60b6ea1a0d6c5eaa0928645e345a800f413e351
Candice1121 <75481901+Candice1121@users.noreply.github.com>
2021-07-15 15:38:02 +0800

    First commit

---
+++ ada/ada/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ada
+//
+//  Created by 菜菜的VegBook on 7/15/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
